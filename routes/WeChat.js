5f23a4894aa1b6f5b81cf19736c387c0703ce30f
Twx1213 <30528285+Twx1213@users.noreply.github.com>
2019-04-01 16:19:13 +0800

    添加【帮助】、【课程表】、【作业】、【作业1】回复内容

--- routes/WeChat.js
+++ routes/WeChat.js
@@ -3,6 +3,7 @@ var router = express.Router();
 //wechat
 var configs = require('../config');
 var wechat = require('wechat');
+var rf=require("fs");
 
 //crypto
 var crypto = require('crypto');
@@ -16,7 +17,30 @@ var config = {
 router.all('/', wechat(config, function (req, res, next) {
     // 微信输入信息都在req.weixin上
     var message = req.weixin;
-    res.reply('公众号架构更新中');
+    var content='公众号功能更新中';
+
+    if(message.Content === "课程表"){
+        res.reply({
+            type: "image",
+            content: {
+                mediaId: 'xotx8ge9KFo44o10hUQ8PmMr_9wrVpomh7sZYyfWpuJ8ORn0gEeVwO0epmTqzH5U'
+            }
+        });
+    }else if(message.Content === "作业"){
+        content = rf.readFileSync("public/text/homework","utf-8");
+        res.reply(content);
+    }else if(message.Content === "作业1"){
+        content = rf.readFileSync("public/text/homework1","utf-8");
+        res.reply(content);
+    }else if(message.Content === "帮助"){
+        content = rf.readFileSync("public/text/help","utf-8");
+        res.reply(content);
+    }else if(message.Content === "后端代码"){
+        content = 'https://github.com/Twx1213/WeChat805';
+        res.reply(content);
+    }else{
+        res.reply(content);
+    }
 }));
 
 module.exports = router;
