e4a60a5ee14dad515250d1bdeffa55afc2b1b873
Twx1213 <30528285+Twx1213@users.noreply.github.com>
2019-04-02 15:33:04 +0800

    加点沙雕功能？

--- routes/WeChat.js
+++ routes/WeChat.js
@@ -13,33 +13,51 @@ var config = {
     encodingAESKey: configs.EncodingAESKey,
     checkSignature: false
 };
+var pics = ['stBEFudMK9Unn2euiY4xYvTdRnpV7XfNsHbMEyZAsnTxJ6lSZQJRMQXEs0HZoBdN',
+            'C8MBMQ5GNXsQdy0Y5DucRsryOJczxp2TmBZ8ayvlXJ6qF3WDeV502Ge4r_BTJoxW',
+            'gA0rBAlMLNDrudr-2jYDZhgMozAKZlHb80LfwJ3AyR6H4pl3D815rmAI75mH-Caq',
+            ];
 
 router.all('/', wechat(config, function (req, res, next) {
     // 微信输入信息都在req.weixin上
     var message = req.weixin;
     var content='公众号功能更新中';
 
-    if(message.Content === "课程表"){
-        res.reply({
-            type: "image",
-            content: {
-                mediaId: 'xotx8ge9KFo44o10hUQ8PmMr_9wrVpomh7sZYyfWpuJ8ORn0gEeVwO0epmTqzH5U'
-            }
-        });
-    }else if(message.Content === "作业"){
-        content = rf.readFileSync("public/text/homework","utf-8");
-        res.reply(content);
-    }else if(message.Content === "作业1"){
-        content = rf.readFileSync("public/text/homework1","utf-8");
-        res.reply(content);
-    }else if(message.Content === "帮助"){
-        content = rf.readFileSync("public/text/help","utf-8");
-        res.reply(content);
-    }else if(message.Content === "后端代码"){
-        content = 'https://github.com/Twx1213/WeChat805';
-        res.reply(content);
-    }else{
-        res.reply(content);
+    switch(message.Content){
+        case "课程表":
+            res.reply({
+                type: "image",
+                content: {
+                    mediaId: 'xotx8ge9KFo44o10hUQ8PmMr_9wrVpomh7sZYyfWpuJ8ORn0gEeVwO0epmTqzH5U'
+                }
+            });
+            break;
+        case "沙雕图":
+            res.reply({
+                type: "image",
+                content: {
+                    mediaId: pics[Math.floor(Math.random()*pics.length)]
+                }
+            });
+            break;
+        case "作业":
+            content = rf.readFileSync("public/text/homework","utf-8");
+            res.reply(content);
+            break;
+        case "作业1":
+            content = rf.readFileSync("public/text/homework1","utf-8");
+            res.reply(content);
+            break;
+        case "帮助":
+            content = rf.readFileSync("public/text/help","utf-8");
+            res.reply(content);
+            break;
+        case "后端代码":
+            content = 'https://github.com/Twx1213/WeChat805';
+            res.reply(content);
+            break;
+        default:
+            res.reply(content);
     }
 }));
 
